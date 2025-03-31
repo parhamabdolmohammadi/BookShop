6939ecc00c79397a532f40ace24539e1cee2bd2d
Danton Soares <dantonvsoares@gmail.com>
2025-03-31 04:07:38 -0700

    Implementing business logic state holders for our players and teams

--- app/src/main/java/com/bcit/termproject2/PlayersState.kt
+++ app/src/main/java/com/bcit/termproject2/PlayersState.kt
@@ -1,4 +1,33 @@
 package com.bcit.termproject2
 
-class PlayersState {
+import androidx.compose.runtime.toMutableStateList
+
+class PlayersState(private val repository: Repository) {
+    var players = repository.getAllPlayers().toMutableStateList()
+
+    fun addPlayer(player: Player) {
+        players.add(player)
+        repository.insertPlayer(player)
+    }
+    fun removePlayer(player: Player) {
+        players.remove(player)
+        repository.deletePlayer(player)
+    }
+    fun refreshPlayers() {
+        clear()
+        addAll(repository.getAllPlayers())
+    }
+}
+
+class TeamsState(private val repository: Repository) {
+    var teams = repository.getAllTeams().toMutableStateList()
+
+    fun addTeam(team: Team) {
+        teams.add(team)
+        repository.insertTeam(team)
+    }
+    fun removeTeam(team: Team) {
+        teams.remove(team)
+        repository.deleteTeam(team)
+    }
 }
