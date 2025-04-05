44817cb289067983b66ef838e4c9098e2a04400c
Parham Abdolmohammadi <parham.abdo@gmail.com>
2025-04-04 23:05:17 -0700

    finishing main activity

--- app/src/main/java/com/bcit/termproject2/MainActivity.kt
+++ app/src/main/java/com/bcit/termproject2/MainActivity.kt
@@ -2,11 +2,14 @@ package com.bcit.termproject2
 
 
 
+//operation done
+import Library
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.animation.animateContentSize
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
@@ -23,16 +26,21 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Info
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material.icons.filled.Search
 import androidx.compose.material.icons.filled.Star
 import androidx.compose.material.icons.outlined.SportsSoccer
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -41,34 +49,66 @@ import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.NavigationBar
 import androidx.compose.material3.NavigationBarItem
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
+import com.bcit.lecture10bby.data.com.bcit.termproject2.BookState
+
+
+
+import com.bcit.lecture10bby.data.com.bcit.termproject2.data.BookRepository
+import com.bcit.lecture10bby.data.com.bcit.termproject2.data.client
 import com.bcit.termproject2.ui.theme.TermProject2Theme
+import android.annotation.SuppressLint
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
+import coil3.compose.AsyncImage
+
+import kotlinx.coroutines.launch
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 
 class MainActivity : ComponentActivity() {
     // Declaring db objects
     private val db by lazy { Database.getInstance(applicationContext) }
     private val repo by lazy { Repository(db.teamDao(), db.playerDao()) }
+
+    val bookRepository by lazy {
+        BookRepository(client)
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -76,43 +116,55 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
 
-            if (repo.getAllTeams().isEmpty()) {
-                val mockPlayers = listOf(
-                    Player(0, "Lionel Messi", "Forward", 10, 0, "Argentina", "1987-06-24", 700),
-                    Player(0, "Neymar", "Forward", 11, 0, "Brazil", "1992-02-05", 400),
-                    Player(0, "Cristiano Ronaldo", "Forward", 7, 0, "Portugal", "1985-02-05", 800)
-                )
 
-                val team = Team(
-                    id = 0,
-                    name = "Barcelona",
-                    country = "Spain",
-                    wins = 30,
-                    losses = 5,
-                    ties = 3,
-                )
 
-                repo.insertTeam(team)
+            MainContent(bookRepository)
 
-                mockPlayers.forEach { player ->
-                    repo.insertPlayer(player.copy(teamId = team.id))
-                }
-            }
-            MainContent()
+//            if (repo.getAllTeams().isEmpty()) {
+//                val mockPlayers = listOf(
+//                    Player(0, "Lionel Messi", "Forward", 10, 0, "Argentina", "1987-06-24", 700),
+//                    Player(0, "Neymar", "Forward", 11, 0, "Brazil", "1992-02-05", 400),
+//                    Player(0, "Cristiano Ronaldo", "Forward", 7, 0, "Portugal", "1985-02-05", 800)
+//                )
+//
+//                val team = Team(
+//                    id = 0,
+//                    name = "Barcelona",
+//                    country = "Spain",
+//                    wins = 30,
+//                    losses = 5,
+//                    ties = 3,
+//                )
+//
+//                repo.insertTeam(team)
+//
+//                mockPlayers.forEach { player ->
+//                    repo.insertPlayer(player.copy(teamId = team.id))
+//                }
+//            }
+
         }
     }
 }
+
+
 
 data class NavItem(val icon: ImageVector, val route:String)
 
 
 // main content of the program
 @Composable
-fun MainContent() {
+fun MainContent(bookRepository: BookRepository) {
+
     //take use somewhere
     //remember the previous state(page) in back button
     //it adds transition (much better that usual) with good flow
     val navController =  rememberNavController()
+
+    val bookState = viewModel {
+        BookState(bookRepository)
+    }
+
 
     Scaffold (
         topBar = {
@@ -129,30 +181,46 @@ fun MainContent() {
             "home",
             modifier = Modifier.padding(padding)) {
 
+
             //Navigation Graphs
             //destination 1
             //rout is id
             composable("home") {
                 //what is ui that we wanna use for this destination
-//                Home(navController) //REMOVED
+                HomeContent(navController, bookRepository)
+//                Books(navController, bookRepository, "persia")
 
-                MyCard(navController)
             }
 
-            //destination 2
-            composable("info/{name}/{image}") {
-                val name = it.arguments?.getString("name")
-                val image = it.arguments?.getString("image")?.toIntOrNull()
-//                Info(name,image)
-                Text("info", fontSize = 12.sp)
+            composable("library") {
+                //what is ui that we wanna use for this destination
+                Library(navController, bookRepository)
             }
 
-            //destination3
-            composable("search"){
-                Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
-                    Text("Search", fontSize = 30.sp)
-                }
+
+//            composable("books", ) {
+//                Books(navController, bookRepository)
+//            }
+            composable("books/{query}") { backStackEntry ->
+                val query = backStackEntry.arguments?.getString("query") ?: ""
+                println("we've got ${query}")
+                Books(navController, bookRepository, query)
             }
+
+            composable(
+                route = "book/{title}/{author}/{year}/{description}/{image1}"
+            ) { backStackEntry ->
+                val title = backStackEntry.arguments?.getString("title") ?: ""
+                val author = backStackEntry.arguments?.getString("author") ?: ""
+                val year = backStackEntry.arguments?.getString("year") ?: ""
+                val description = backStackEntry.arguments?.getString("description") ?: ""
+                var image2 = backStackEntry.arguments?.getString("image1")?: ""
+                image2 = image2.replace("http://", "https://",)
+
+                Book(title = title, author = author, year = year, description = description, image = image2)
+            }
+
+
 
             //destination4
             composable("more"){
@@ -196,7 +264,7 @@ fun MyBottomNav(navController: NavController) {
 
     val navItems = listOf(
         NavItem(Icons.Default.Home, "home"),
-        NavItem(Icons.Default.Search, "search"),
+        NavItem(Icons.Default.Search, "library"),
         NavItem(Icons.Default.Menu, "more"),
     )
 
@@ -223,263 +291,25 @@ fun MyBottomNav(navController: NavController) {
 
 
 
-@Composable
-fun MyCard(navController: NavController) {
-    val scrollState = rememberScrollState()
-
-    Column(
-        modifier = Modifier
-            .fillMaxSize()
-            .verticalScroll(scrollState)
-            .padding(16.dp)
-    ) {
-        ClickableBoxWithSubBoxes()
-        Spacer(modifier = Modifier.height(16.dp))
-        ClickableBoxWithSubBoxes2()
-    }
-}
-
-
-@Composable
-fun ClickableBoxWithSubBoxes() {
-    var showSubBoxes = remember { mutableStateOf(false) }
-
-    Box(
-        modifier = Modifier
-            .wrapContentSize() // 👈 Make the Box size itself to its content
-            .clickable {
-                showSubBoxes.value = !showSubBoxes.value // Toggle visibility
-            }
-            .animateContentSize() // 👈 Smooth resizing animation
-    ) {
-
-
-        Column(
-            modifier = Modifier
-                .align(Alignment.Center)
-                .padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(12.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Box(){
-                Image(
-                    painter = painterResource(id = R.drawable.soccerplayerss),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .fillMaxWidth()// Optional: define size of the image
-                        .clip(RoundedCornerShape(10.dp))
-                )
-            }
-
-            if (showSubBoxes.value) {
-
-                Row(modifier = Modifier
-                    .fillMaxWidth()
-                    .size(120.dp)
-                    .clip(RoundedCornerShape(10.dp))
-                    .background(Color.LightGray)
-
-                    ,
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically) {
-
-                    Box(modifier = Modifier.padding(10.dp)){
-                        Image(
-                            painter = painterResource(id = R.drawable.messi),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(130.dp)
-
-                                .clip(RoundedCornerShape(30.dp)))
 
 
 
-                    }
-
-                    Text("Lionel Messi", fontSize = 25.sp)
-                    Spacer(modifier = Modifier.width(200.dp))
-                }
-
-                Row(modifier = Modifier
-                    .fillMaxWidth()
-                    .size(120.dp)
-                    .clip(RoundedCornerShape(10.dp))
-                    .background(Color.LightGray)
-
-                    ,
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                    verticalAlignment = Alignment.CenterVertically) {
-
-                    Box(modifier = Modifier.padding(10.dp)){
-                        Image(
-                            painter = painterResource(id = R.drawable.ronaldo),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(120.dp)
-                                .fillMaxSize()
-                                .clip(RoundedCornerShape(30.dp)))
 
 
 
-                    }
-
-                    Text("Ronaldo", fontSize = 25.sp)
-                    Spacer(modifier = Modifier.width(200.dp))
-                }
-
-                Row(modifier = Modifier
-                    .fillMaxWidth()
-                    .size(120.dp)
-                    .clip(RoundedCornerShape(10.dp))
-                    .background(Color.LightGray)
-
-                    ,
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically) {
-
-                    Box(modifier = Modifier.padding(10.dp)){
-
-                        Image(
-                            painter = painterResource(id = R.drawable.neymar),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(130.dp)
-                                .fillMaxHeight()
-                                .clip(RoundedCornerShape(30.dp)))
 
 
 
-                    }
-
-                    Text("Neymar", fontSize = 25.sp)
-                    Spacer(modifier = Modifier.width(200.dp))
-                }
-            }
-            else {
-                Text("Players", fontSize = 30.sp)
-            }
-        }
-    }
-}
-
-@Composable
-fun ClickableBoxWithSubBoxes2() {
-    var showSubBoxes = remember { mutableStateOf(false) }
-
-    Box(
-        modifier = Modifier
-            .wrapContentSize() // 👈 Make the Box size itself to its content
-            .clickable {
-                showSubBoxes.value = !showSubBoxes.value // Toggle visibility
-            }
-            .animateContentSize() // 👈 Smooth resizing animation
-    ) {
-
-
-        Column(
-            modifier = Modifier
-                .align(Alignment.Center)
-                .padding(16.dp),
-            verticalArrangement = Arrangement.spacedBy(12.dp),
-            horizontalAlignment = Alignment.CenterHorizontally
-        ) {
-            Box(){
-                Image(
-                    painter = painterResource(id = R.drawable.teams),
-                    contentDescription = null,
-                    modifier = Modifier
-                        .fillMaxWidth()// Optional: define size of the image
-                        .clip(RoundedCornerShape(10.dp))
-                )
-            }
-
-            if (showSubBoxes.value) {
-
-                Row(modifier = Modifier
-                    .fillMaxWidth()
-                    .size(120.dp)
-                    .clip(RoundedCornerShape(10.dp))
-                    .background(Color.LightGray)
-
-                    ,
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically) {
-
-                    Box(modifier = Modifier.padding(10.dp)){
-                        Image(
-                            painter = painterResource(id = R.drawable.barcelona),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(130.dp)
-
-                                .clip(RoundedCornerShape(30.dp)))
 
 
 
-                    }
-
-                    Text("Barcelona", fontSize = 25.sp)
-                    Spacer(modifier = Modifier.width(200.dp))
-                }
-
-                Row(modifier = Modifier
-                    .fillMaxWidth()
-                    .size(120.dp)
-                    .clip(RoundedCornerShape(10.dp))
-                    .background(Color.LightGray)
-
-                    ,
-                    horizontalArrangement = Arrangement.SpaceEvenly,
-                    verticalAlignment = Alignment.CenterVertically) {
-
-                    Box(modifier = Modifier.padding(10.dp)){
-                        Image(
-                            painter = painterResource(id = R.drawable.intermilan),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(120.dp)
-                                .fillMaxSize()
-                                .clip(RoundedCornerShape(30.dp)))
 
 
 
-                    }
-
-                    Text("Inter Milan", fontSize = 25.sp)
-                    Spacer(modifier = Modifier.width(200.dp))
-                }
-
-                Row(modifier = Modifier
-                    .fillMaxWidth()
-                    .size(120.dp)
-                    .clip(RoundedCornerShape(10.dp))
-                    .background(Color.LightGray)
-
-                    ,
-                    horizontalArrangement = Arrangement.SpaceBetween,
-                    verticalAlignment = Alignment.CenterVertically) {
-
-                    Box(modifier = Modifier.padding(10.dp)){
-
-                        Image(
-                            painter = painterResource(id = R.drawable.liverpool),
-                            contentDescription = null,
-                            modifier = Modifier
-                                .size(130.dp)
-                                .fillMaxHeight()
-                                .clip(RoundedCornerShape(30.dp)))
 
 
 
-                    }
 
-                    Text("LiverPool", fontSize = 25.sp)
-                    Spacer(modifier = Modifier.width(200.dp))
-                }
-            }
-            else {
-                Text("Teams", fontSize = 30.sp)
-            }
-        }
-    }
-}
+
+
+
